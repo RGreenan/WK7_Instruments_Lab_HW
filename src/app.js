2583573745628899293610dc7c35bd2e0daffc06
Ray <ray.greenan.89@outlook.com>
2018-11-27 17:16:27 +0000

    Started file structure

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const InstrumentFamilies = require('./models/instrument_families.js')
+const InstrumentFamilyView = require('./views/instrument_family_view.js')
+const SelectView = require('./views/select_view.js')
+
+document.addEventListener('DOMContentLoaded', () => {
+  console.log('JavaScript Loaded');
+
+  
+  // const instrumentFamilyModel = new InstrumentFamilyView()
+  // instrumentFamilyModel.bindEvents();
+
+});
---
+++ src/views/instrument_family_view.js
@@ -0,0 +1,9 @@
+const PubSub = require('../helpers/pub_sub.js');
+
+const InstrumentFamilyView = function () {
+
+}
+
+// TODO: BIND EVENTS
+
+module.exports = InstrumentFamilyView;
