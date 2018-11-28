6df11aaad1a1061eb5abe66ef2c84e77a7cc9aa1
Ray <ray.greenan.89@outlook.com>
2018-11-28 08:59:41 +0000

    one file change

--- src/views/instrument_family_view.js
+++ src/views/instrument_family_view.js
@@ -3,7 +3,14 @@ const PubSub = require('../helpers/pub_sub.js');
 const InstrumentFamilyView = function () {
 
 }
-
 // TODO: BIND EVENTS
+
+InstrumentFamilyView.prototype.bindEvents = function () {
+  PubSub.publish();
+
+  PubSub.subscribe( () => {
+
+  });
+};
 
 module.exports = InstrumentFamilyView;
